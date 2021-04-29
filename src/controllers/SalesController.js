8303e5f095d1ae45ced0c531f20cd8236947d85e
Lucas Ferreira <lbrandof@gmail.com>
2021-04-29 10:37:26 -0300

    Add a SalesController

---
+++ src/controllers/SalesController.js
@@ -0,0 +1,13 @@
+const knex = require('../database/db')
+
+module.exports = {
+
+    async index(req, res) {
+        try {
+            await res.json({})
+        } catch {
+
+        }
+    }
+
+}
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ const routes = express.Router()
 
 const ClientController = require('./controllers/ClientController')
 const ProductController = require('./controllers/ProductController')
+const SalesController = require('./controllers/SalesController')
 
+// Routes Client
 routes.get('/clients', ClientController.index)
 routes.post('/clients', ClientController.create)
+
+//Route Products
 routes.get('/products', ProductController.index)
 
 
+//Route Products
+routes.get('/sales', SalesController.index)
 module.exports = routes
