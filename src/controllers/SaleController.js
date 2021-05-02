519fd755da39bdbba42ce117e90c5d12ae7f7d90
Lucas Ferreira <lbrandof@gmail.com>
2021-05-02 17:25:03 -0300

    Insert joins and select on lastsales

--- src/controllers/SaleController.js
+++ src/controllers/SaleController.js
@@ -32,7 +32,11 @@ module.exports = {
     async lastsale(req, res, next) {
         try {
             const { maxrows } = req.query
-            const results = await knex('sale').limit(maxrows).offset(maxrows)
+            const results = await knex('sale')
+            .join('customer', 'sale.id_client', '=', 'customer.id_customer')
+            .join('product', 'sale.id_product', '=', 'product.id_product')
+            .select('sale.id_sale', 'customer.name_customer', 'product.name_product', 'sale.sale', 'sale.date_sale', 'sale.price_product', 'sale.created_at')
+            .limit(maxrows)
             return res.json(results)
         } catch (error) {
             next(error)
